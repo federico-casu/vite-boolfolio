8debc25d379c18b9e62a2d52201fee9652513635
federico-casu <federico.casu1101@gmail.com>
2024-04-05 14:18:59 +0200

    feat: added api call to backend to get all projects info; added Main component and Project Card component to represent each project

---
+++ src/components/MainComp.vue
@@ -0,0 +1,50 @@
+
+
+<script>
+
+    import ProjectCard from './ProjectCard.vue';
+
+    // Js componente
+    export default {
+        name: 'MainComp',
+        components: {
+            ProjectCard
+        },
+        props: {
+            projectsProp: Array
+        },
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+
+<template>
+    <!-- html componente -->
+    <!-- <ul>
+        <li
+        v-for="(project, index) in this.projectsProp"
+        :key="project.id"
+        >{{ project.title }}</li>
+    </ul> -->
+
+    <ProjectCard 
+    v-for="(project, index) in this.projectsProp"
+    :key="project.id"
+    :projectProp="project"
+    />
+    
+</template>
+
+
+<style scoped>
+    /* stile del componente */
+
+    
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+// Js componente
+export default {
+    name: 'ProjectCard',
+    props: {
+        projectProp: Object
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+
+<template>
+    <!-- html componente -->
+    <!-- <h1>Ciao dalla ProjectCard</h1> -->
+
+    <div class="card">
+        <img class="card-img-top" :src="'http://127.0.0.1:8000/storage/' + this.projectProp.cover_image"
+            :alt="this.projectProp.title" />
+        <div class="card-body">
+            <h4 class="card-title">{{ this.projectProp.title }}</h4>
+            <p class="card-text">{{ this.projectProp.description }}</p>
+        </div>
+    </div>
+
+
+</template>
+
+
+<style scoped>
+/* stile del componente */
+</style>
